d220fdc57dff175183f8b90db0afed8ddd611b59
hoyeong-s <71427588+hoyeong-s@users.noreply.github.com>
2021-04-27 14:53:21 +0900

    0427

--- Study/week4/back_5904.java
+++ Study/week4/back_5904.java
@@ -17,18 +17,17 @@ public class back_5904 {
 	}
 
 	private static void moo(int s, int cnt) { // 어느 그룹인지 판단
-		if(N < s) {
-			if(dp[cnt-1] >= N-dp[cnt-1]) {
-				N = N-dp[cnt-1];
-				find(N,cnt-1);
-			}
-			else if(N > s-dp[cnt-1]) { // dp[cnt-1]에 속해있는지 판단
+		if(N <= s) {
+			if(N > dp[cnt-1]+(3+cnt)) { // 3그룹에 있을 때
 				N = N-dp[cnt-1]-(3+cnt);
-				find(N,cnt-1);
+				find(cnt-1);
 			}
-			else { // 새로 만든 문자열에 속해있는지 판단
+			else if(N > dp[cnt-1]) { // 2그룹
 				if(N == dp[cnt-1]+1) System.out.println("m");
 				else System.out.println("o");
+			}
+			else { // 1그룹
+				find(cnt-1);
 			}
 		}
 		else {
@@ -37,24 +36,22 @@ public class back_5904 {
 		}
 	}
 	
-	private static void find(int s,int cnt) {
+	private static void find(int cnt) {
 		if(cnt==0) {
-			if(s==1) System.out.println("m");
+			if(N==1) System.out.println("m");
 			else System.out.println("o");
 			return;
 		}
-		
-		if(N <= dp[cnt-1]) {
-			find(N,cnt-1);
-		}
-			
-		else if(N > s-dp[cnt-1]) { // dp[cnt-1]에 속해있는지 판단
+		if(N > dp[cnt-1]+(3+cnt)) { // 3그룹에 있을 때
 			N = N-dp[cnt-1]-(3+cnt);
-			find(N,cnt-1);
+			find(cnt-1);
 		}
-		else { // 새로 만든 문자열에 속해있는지 판단
+		else if(N > dp[cnt-1]) { // 2그룹
 			if(N == dp[cnt-1]+1) System.out.println("m");
 			else System.out.println("o");
+		}
+		else { // 1그룹
+			find(cnt-1);
 		}
 	}
 }
