7d4ba4367ccbe651cc2be287b730c102e00f2802
hoyeong-s <71427588+hoyeong-s@users.noreply.github.com>
2021-07-29 21:55:06 +0900

    0729

---
+++ ESC/cs1/back_2281.java
@@ -0,0 +1,19 @@
+package cs1;
+
+import java.io.*;
+import java.util.*;
+
+public class back_2281 {
+	static int N,M;
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		
+	}
+
+}
