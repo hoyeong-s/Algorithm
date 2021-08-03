7366e52917baa47b788704f2c27925b2c5c62baa
hoyeong-s <71427588+hoyeong-s@users.noreply.github.com>
2021-08-03 23:48:34 +0900

    0803

--- ESC/cs1/back_2281.java
+++ ESC/cs1/back_2281.java
@@ -4,7 +4,8 @@ import java.io.*;
 import java.util.*;
 
 public class back_2281 {
-	static int N,M;
+	static int N,M,result = 987654321;
+	static int [] name, save;
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,8 +13,27 @@ public class back_2281 {
 		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
+		name = new int [N];
+		save = new int [N];
+		Arrays.fill(save, 987654321);
 		
+		for(int i=0; i<N; i++) 
+			name[i] = Integer.parseInt(br.readLine());
 		
+		note(1,0,name[0]);
+		System.out.println(result);
 	}
-
+	private static void note(int idx, int sum, int low) {
+		if(idx>=N) {
+			result = Math.min(result, sum);
+			return;
+		}
+		
+		int cal = low+1+name[idx]; // 현재 줄에서 지금까지 idx
+		if(cal<=M) { // 그 줄에 넣는 경우
+			note(idx+1,sum,cal);
+		}
+		sum += Math.pow(M-low,2);
+		note(idx+1,sum,name[idx]);
+	}
 }
