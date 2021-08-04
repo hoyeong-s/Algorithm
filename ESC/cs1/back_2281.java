e57fff8094b0d904928c31713e1bb2904840f66e
hoyeong-s <71427588+hoyeong-s@users.noreply.github.com>
2021-08-05 03:00:19 +0900

    0805

--- ESC/cs1/back_2281.java
+++ ESC/cs1/back_2281.java
@@ -5,7 +5,8 @@ import java.util.*;
 
 public class back_2281 {
 	static int N,M,result = 987654321;
-	static int [] name, save;
+	static int [] name;
+	static int [][] dp;
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,26 +15,29 @@ public class back_2281 {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		name = new int [N];
-		save = new int [N];
-		Arrays.fill(save, 987654321);
+		dp = new int [N][M+1];
+		for(int [] row : dp)
+			Arrays.fill(row, -1);
 		
 		for(int i=0; i<N; i++) 
 			name[i] = Integer.parseInt(br.readLine());
 		
-		note(1,0,name[0]);
-		System.out.println(result);
+		System.out.println(note(1,name[0]));
 	}
-	private static void note(int idx, int sum, int low) {
-		if(idx>=N) {
-			result = Math.min(result, sum);
-			return;
-		}
+	// top-down 형식으로 끝에서부터 계산
+	private static int note(int idx, int row) { // row : 현재까지 그 줄의 채워진 칸의 길이
+		if(idx>=N) return 0;
 		
-		int cal = low+1+name[idx]; // 현재 줄에서 지금까지 idx
-		if(cal<=M) { // 그 줄에 넣는 경우
-			note(idx+1,sum,cal);
+		if(dp[idx][row]!=-1) return dp[idx][row];
+		
+		int cal = M - row;
+		dp[idx][row] = note(idx+1,name[idx]) + (int)Math.pow(cal,2); // 다음줄에 넣음
+		
+		cal = row + 1 + name[idx];
+		if(cal<=M) { // 해당 줄에 넣을 수 있는 경우
+			dp[idx][row] = Math.min(dp[idx][row], note(idx+1, cal));
 		}
-		sum += Math.pow(M-low,2);
-		note(idx+1,sum,name[idx]);
+					
+		return dp[idx][row];
 	}
 }
