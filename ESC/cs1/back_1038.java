7319f62ea487d97e1c46ebf6dbdd77db7cccdaba
hoyeong-s <71427588+hoyeong-s@users.noreply.github.com>
2021-08-20 22:15:15 +0900

    0820

--- ESC/cs1/back_1038.java
+++ ESC/cs1/back_1038.java
@@ -1,10 +1,42 @@
 package cs1;
 
-public class back_1493 {
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
 
-	public static void main(String[] args) {
+public class back_1038 {
+	static int N;
+	static Queue<Long> q = new LinkedList<>();
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
-
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		if(N<10) System.out.println(N);
+		else {
+			int cnt=0;
+			for(int i=1; i<10; i++) q.add((long) i); // 초기설정
+			while(true) {
+				long num = q.poll();
+				cnt++;
+				if(N<=cnt || num==-1) {
+					System.out.println(num);
+					break;
+				}
+				make(num);
+			}
+				
+		}
 	}
-
+	private static void make(Long num) {
+		Long last = num % 10;
+		num = num * 10;
+		if(num > 9876543210L) {
+			q.add((long) -1);
+			return;
+		}
+		for(int i=0; i<last; i++)
+			q.add(num+i);
+	}
 }
