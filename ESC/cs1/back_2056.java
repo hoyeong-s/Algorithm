4baa10a418a5860480fe10797b0213d3354bf2b5
hoyeong-s <71427588+hoyeong-s@users.noreply.github.com>
2021-09-02 00:59:04 +0900

    0902

--- ESC/cs1/back_2056.java
+++ ESC/cs1/back_2056.java
@@ -4,9 +4,10 @@ import java.io.*;
 import java.util.*;
 
 public class back_2056 {
-	static int N;
+	static int N,max;
 	static int [] time;
-	static ArrayList<Integer> [] list;
+	static ArrayList<Integer> start = new ArrayList<>();
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,21 +16,23 @@ public class back_2056 {
 		N = Integer.parseInt(br.readLine());
 		
 		time = new int [N+1];
-	
-		list = new ArrayList[N+1];
-		for(int i=1; i<=N; i++)
-			list[i] = new ArrayList<>();
-		
+			
 		for(int i=1; i<=N; i++) {
 			st = new StringTokenizer(br.readLine());
 			int t = Integer.parseInt(st.nextToken());
+			int num = 0, mm = 0;
+			
 			int n = Integer.parseInt(st.nextToken());
 			for(int j=0; j<n; j++) {
-				int num = Integer.parseInt(st.nextToken());
-				list[i].add(num);
+				num = Integer.parseInt(st.nextToken());
+				mm = Math.max(mm, time[num]);
 			}
+
+			time[i] = mm + t;
+			max = Math.max(max, time[i]);
 		}
 		
+		System.out.println(max);
 		
 	}
 
