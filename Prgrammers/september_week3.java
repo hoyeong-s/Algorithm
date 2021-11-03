d3e320f6a63f899e89ec01ed2ec50806dec96b34
hoyeong-s <71427588+hoyeong-s@users.noreply.github.com>
2021-11-04 00:18:13 +0900

    1104

--- Prgrammers/september_week3.java
+++ Prgrammers/september_week3.java
@@ -1,13 +1,16 @@
 import java.io.*;
 import java.util.*;
 
+
 public class september_week3 {
-	static int N,result,number;
-	static int [] dr = {0,0,0,-1,1}; // 좌우상하
-	static int [] dc = {0,-1,1,0,0};
-	static ArrayList<int []> [] list1; // table의 순서도가 들어갈 리스트
-	static boolean [] check;
+	static int N,number,result;
 	static boolean [][] visited;
+	static int [] dr = {0,0,-1,1}; // 좌우상하
+	static int [] dc = {-1,1,0,0};
+	static int [][] board;
+	static boolean [] check, check1;
+	static HashMap<Integer,String> map = new HashMap<>();
+	
 	static class Dot{
 		int r,c;
 
@@ -19,71 +22,116 @@ public class september_week3 {
 	}
 	
 	public static void main(String[] args) {
-
-int[][] game_board = {{0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0}, {1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0}, {0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0}, {1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1}, {0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0}, {0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1}, {0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0}, {0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0}, {1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0}, {0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0}, {0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1}, {0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0}}; 
-int[][] table = {{1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1},{1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1},{1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0},{0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0},{1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0},{1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},{1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1},{1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1}, {0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1}, {1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1}, {1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1}, {1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1}}; 
-
-solution(game_board,table);
+		int[][] game_board = {{0,0,1,0,1,0,1,0,1,0,1,0,0,1,0,0,0,0}, {1,0,0,0,1,0,1,0,1,0,1,0,0,1,0,1,1,1}, {0,1,1,1,0,0,1,0,1,0,0,1,1,0,1,0,0,0}, {0,0,0,0,1,1,0,0,1,1,0,1,0,0,1,0,0,0}, {0,1,1,1,0,0,1,1,1,1,0,1,1,1,0,1,1,1}, {1,0,1,0,0,0,1,0,0,0,1,0,0,0,0,1,0,0}, {0,0,0,1,1,1,0,0,1,1,0,1,1,1,1,0,0,1}, {1,1,1,0,0,0,1,1,0,0,1,0,0,0,0,1,1,0}, {0,0,1,0,1,1,1,0,0,1,0,1,1,1,1,0,0,0}, {1,1,0,1,1,0,1,1,1,1,0,1,0,0,0,1,1,1}, {0,0,0,0,1,0,0,0,0,1,0,1,0,0,1,0,1,0}, {1,1,1,1,0,1,1,1,1,1,0,1,0,1,0,0,1,0}, {0,0,1,0,0,0,1,0,0,0,1,0,1,0,1,1,0,0}, {1,0,1,1,0,1,1,0,0,0,1,0,0,0,1,0,0,1}, {1,0,0,1,1,0,0,1,1,1,0,1,1,1,0,1,1,0}, {0,1,1,0,0,1,0,1,0,0,1,0,0,0,0,0,1,0}, {0,0,0,1,0,1,0,1,0,0,1,1,1,1,1,1,1,0}, {0,1,0,1,1,0,0,1,0,1,0,0,0,0,0,0,1,0}}; int[][] table = {{1,1,1,1,1,1,0,1,0,1,1,0,0,1,0,0,1,0}, {0,0,0,0,0,0,1,1,1,0,1,0,1,1,0,1,1,0}, {1,0,1,1,0,1,0,1,0,1,1,0,1,0,1,1,0,1}, {1,1,0,1,1,1,0,1,0,1,0,1,1,0,1,0,0,1}, {1,1,1,0,0,0,1,0,1,0,1,0,0,1,0,0,1,1}, {0,0,0,1,1,1,0,1,1,1,0,1,1,0,1,0,0,0}, {1,1,1,0,0,0,0,0,1,1,0,1,1,0,1,1,1,1}, {0,0,1,0,1,1,0,1,0,0,1,0,0,1,0,0,0,0}, {1,0,1,0,0,0,0,1,0,1,1,0,1,1,0,1,1,1}, {1,0,1,0,1,1,1,1,0,1,1,0,0,0,1,1,1,0}, {1,1,0,1,0,0,0,0,1,0,0,1,1,1,0,0,0,0}, {0,0,1,1,1,1,0,1,1,0,1,0,0,0,1,1,0,1}, {1,1,0,1,0,0,1,0,0,1,0,1,0,1,0,1,0,1}, {1,1,0,0,1,1,1,0,1,1,0,1,0,1,0,1,0,1}, {0,0,1,1,0,1,1,0,1,0,1,1,0,0,0,1,0,0}, {1,1,1,0,1,0,0,1,0,1,1,0,0,1,0,1,0,1}, {0,0,0,0,1,0,1,1,1,0,0,1,0,1,1,0,1,1}, {0,1,1,1,1,0,0,1,0,0,1,1,0,1,0,0,1,1}}; 
+		solution(game_board,table);
 	}
 	public static int solution(int[][] game_board, int[][] table) {
         int answer = -1;
-        N = table.length;
-        visited = new boolean [N][N];
+        N = game_board.length;
+        board = game_board;
+        visited = new boolean[N][N];
+                
+        for(int i=0; i<N; i++) {
+        	for(int j=0; j<N; j++) {
+        		if(game_board[i][j]==0 && !visited[i][j]) bfs(i,j,game_board,0,0);
+        	}
+        }
         
-        table = find(table); // 도형 번호 메기기
-        
-        list1 = new ArrayList[number+1];
-        for(int i=2; i<=number; i++)
-        	list1[i] = new ArrayList<>();
-        check = new boolean[number];
+        table = find(table); // table 번호 메기기
+        check = new boolean[map.size()];
+        check1 = new boolean[number+1];
         
         for(int t=0; t<4; t++) {
         	for(int i=0; i<N; i++) {
+        		for(boolean [] r : visited)
+        			Arrays.fill(r, false);
+        	
         		for(int j=0; j<N; j++) {
-        			if(table[i][j]!=0 && !visited[i][j]) {
-        				int num = table[i][j];
-        				int [] arr = make(i,j,table);
-        				if(!checking(num,arr)) list1[num].add(arr);
-        			}
+        			if(table[i][j]!=0 && !visited[i][j] && !check1[table[i][j]]) bfs(i,j,table,table[i][j],1);
         		}
         	}
-        	
-        	table = spin(table); // 테이블 회전
-        	for(boolean [] r : visited)
-        		Arrays.fill(r, false);
+        	table = spin(table);
         }
-        
-        for(int i=0; i<N; i++) {
-        	for(int j=0; j<N; j++) {
-        		if(game_board[i][j]==0 && !visited[i][j]) last(i,j,game_board);
-        	}
-        }
-        
         return result;
     }
-	private static int [] make(int rr, int cc, int [][] table) { // 순서도 만들기
-		int cnt = 0;
-		int [] load = new int[5];
+	
+	private static void bfs(int rr, int cc, int [][] game_board, int num, int tb) { // 구조도 만들기
 		Queue<Dot> q = new LinkedList<>();
 		visited[rr][cc] = true;
 		q.add(new Dot(rr,cc));
+		int cnt = 1;
 		
+		int lc=cc,rc=cc,tr=rr,br=rr;
 		while(!q.isEmpty()) {
 			Dot d = q.poll();
 			int r = d.r;
 			int c = d.c;
-			for(int dir=1; dir<=4; dir++) {
+			for(int dir=0; dir<4; dir++) {
 				int nr = r + dr[dir];
 				int nc = c + dc[dir];
 				if(nr<0 || nc<0 || nr>=N || nc>=N) continue;
-				if(table[nr][nc]!=0 && !visited[nr][nc]) {
-					visited[nr][nc] = true;
-					load[cnt++] = dir; 
+				if(game_board[nr][nc]==num && !visited[nr][nc]) {
+					visited[nr][nc]=true;
+					cnt++;
+					
+					lc = Math.min(lc, nc);
+					rc = Math.max(rc, nc);
+					tr = Math.min(tr, nr);
+					br = Math.max(br, nr);
+					
 					q.add(new Dot(nr,nc));
 				}
 			}
 		}
-		return load;
+		box(lc,rc,tr,br,game_board,tb,num,cnt);
+	}
+	private static void box(int lc, int rc, int tr, int br, int [][] board, int tb, int num, int cnt) {
+		int [][] arr = new int [br-tr+1][rc-lc+1];
+		for(int i=tr; i<=br; i++) {
+			for(int j=lc; j<=rc; j++) {
+				arr[i-tr][j-lc] = board[i][j];
+			}
+		}
+		String str = draw(arr,tb);
+		
+		if(tb==0) map.put(map.size(),str); // game_board 구조도 그리기
+		else { // table, game_board와 비교
+			if(map.containsValue(str)) { // 같은 모양 존재
+				for(int i=0; i<map.size(); i++) {
+					if(map.get(i).equals(str) && !check[i]) {
+						check[i] = true;
+						check1[num] = true;
+						result+=cnt;
+						return;
+					}
+				}
+			}
+		}
+	}
+	private static String draw(int [][] arr, int tb) {
+		StringBuilder sb = new StringBuilder();
+		
+		if(tb==0) {
+			for(int i=0; i<arr.length; i++) {
+				for(int j=0; j<arr[i].length; j++) {
+					int num = arr[i][j];
+					if(num!=0) sb.append(1);
+					else sb.append(0);
+				}
+				sb.append("n");
+			}
+		}
+		else {
+			for(int i=0; i<arr.length; i++) {
+				for(int j=0; j<arr[i].length; j++) {
+					int num = arr[i][j];
+					if(num!=0) sb.append(0);
+					else sb.append(1);
+				}
+				sb.append("n");
+			}
+		}
+		return sb.toString();
 	}
 	private static int [][] find(int [][] table) { // table 도형 번호 메기기
 		number = 2;
@@ -97,7 +145,7 @@ solution(game_board,table);
 						Dot d = q.poll();
 						int r = d.r;
 						int c = d.c;
-						for(int dir = 1; dir<=4; dir++) {
+						for(int dir = 0; dir<4; dir++) {
 							int nr = r + dr[dir];
 							int nc = c + dc[dir];
 							if(nr<0 || nc<0 || nr>=N || nc>=N) continue;
@@ -124,57 +172,4 @@ solution(game_board,table);
 		}
 		return ctable;
 	}
-	private static void last(int rr, int cc, int [][] game_board) { // 순서도 만들기
-		int cnt = 0;
-		int [] load = new int[5];
-		Queue<Dot> q = new LinkedList<>();
-		visited[rr][cc] = true;
-		q.add(new Dot(rr,cc));
-		
-		while(!q.isEmpty()) {
-			Dot d = q.poll();
-			int r = d.r;
-			int c = d.c;
-			for(int dir=1; dir<=4; dir++) {
-				int nr = r + dr[dir];
-				int nc = c + dc[dir];
-				if(nr<0 || nc<0 || nr>=N || nc>=N) continue;
-				if(game_board[nr][nc]==0 && !visited[nr][nc]) {
-					visited[nr][nc] = true;
-					load[cnt++] = dir; 
-					q.add(new Dot(nr,nc));
-				}
-			}
-		}
-		
-		for(int i=2; i<=number; i++) {
-			for(int j=0; j<list1[i].size(); j++) {
-				if(Arrays.equals(load, list1[i].get(j))&& !check[i]) {
-					check[i] = true;
-					if(cnt==5) {
-						System.out.println(i);
-						for(int x:list1[i].get(j)) System.out.print(x+" ");
-						System.out.println();
-						for(int x:load) System.out.print(x+" ");
-						System.out.println();
-					}
-					result += cnt+1;
-					return;
-				}
-			}
-		}
-	}
-    private static boolean checking(int num, int [] arr) {
-		for(int i=0; i<list1[num].size(); i++) {
-			if(Arrays.equals(arr, list1[num].get(i))) return true;
-		}
-		return false;
-	}
 }
-
-/*
- * 1. game_board bfs 순서도 만들기
- * 2. table 회전하면서 4개의 순서도 만들기
- * 3. 두개 비교하며 가장 큰 것 선택
- *  
- * */
