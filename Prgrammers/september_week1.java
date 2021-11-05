98da7eae22565534807fff11b5f25d407f04fb20
hoyeong-s <71427588+hoyeong-s@users.noreply.github.com>
2021-11-05 20:27:37 +0900

    1105

---
+++ Prgrammers/september_week1.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class september_week1 {
+	public static void main(String[] args) {
+		
+	}
+	public static long solution(int price, int money, int count) {
+        long answer = -1;
+        
+        long sum = 0;
+        for(int i=1; i<=count; i++) sum+=i;
+        long result = (long)(price * sum);
+        
+        if(money-result<0) return result-money;
+        else return 0;
+    }
+}
