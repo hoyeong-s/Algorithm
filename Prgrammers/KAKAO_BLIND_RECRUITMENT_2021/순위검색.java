f102cf174ea91807160403c4ede7add589763be3
hoyeong-s <71427588+hoyeong-s@users.noreply.github.com>
2021-12-01 23:57:32 +0900

    1201

---
+++ Prgrammers/KAKAO_BLIND_RECRUITMENT_2021/순위검색.java
@@ -0,0 +1,15 @@
+package KAKAO_BLIND_RECRUITMENT_2021;
+
+import java.util.*;
+import java.util.*;
+
+public class 순위검색 {
+
+	public static void main(String[] args) {
+
+	}
+	public static int[] solution(String[] info, String[] query) {
+        int[] answer = { };
+        return answer;
+    }
+}
