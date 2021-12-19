b111d0a3a1a166e87e12de8020c2263e50c71111
hoyeong-s <71427588+hoyeong-s@users.noreply.github.com>
2021-12-19 20:07:41 +0900

    1219

--- Prgrammers/Dev_matching_2021/광고_삽입.java
+++ Prgrammers/Dev_matching_2021/광고_삽입.java
@@ -1,12 +1,17 @@
 package Dev_matching_2021;
 
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class 광고_삽입 {
 	static int [] arr;
+	static Queue<Integer> q = new LinkedList<Integer>();
 	public static void main(String[] args) {
 		String [] logs = {"01:20:15-01:45:14", "00:40:31-01:00:00", "00:25:50-00:48:29", "01:30:59-01:53:29", "01:37:44-02:02:30"};
 		solution("02:03:55","00:14:15",logs);
+		
+//		String [] logss = {"69:59:59-89:59:59", "01:00:00-21:00:00", "79:59:59-99:59:59", "11:00:00-31:00:00"};
+//		solution("99:59:59","25:00:00",logss);
 	}
 	public static String solution(String play_time, String adv_time, String[] logs) {
         String answer = "";
@@ -28,20 +33,36 @@ public class 광고_삽입 {
         
         int time = hh*3600 + mm*60 + ss;
         
-        int max = 0;
-        for(int i=1; i<=arr.length-1; i++) {
-        	arr[i] += arr[i-1];
-        	if(i>=time) max = Math.max(max, arr[i]-arr[i-time]);
-        	if(i==5459 || i==6314) System.out.println(i+" "+arr[i]);
+        long sum = 0;
+        for(int i=0; i<time; i++) {
+        	sum += arr[i];
         }
         
-        int sec = max % 60;
-        int min = max / 60;
+        long max = sum;
+        int result = 0;
+        
+        for(int i=time; i<arr.length; i++) {
+        	sum += arr[i]-arr[i-time];
+        	if(sum > max) {
+        		max = sum;
+        		result = i-time+1;
+        	}
+        }
+        
+        int sec = result % 60;
+        int min = result / 60;
         int hour = min / 60;
         min = min % 60;
         
         StringBuilder sb = new StringBuilder();
-        sb.append(hour + ":" + min + ":" + sec);
+        if(hour<10) sb.append("0"+hour);
+        else sb.append(hour);
+        sb.append(":");
+        if(min<10) sb.append("0"+min);
+        else sb.append(min);
+        sb.append(":");
+        if(sec<10) sb.append("0"+sec);
+        else sb.append(sec);
         
         return sb.toString();
     }
@@ -61,9 +82,8 @@ public class 광고_삽입 {
 		
 		int end = hh*3600 + mm*60 + ss;
 		
-		for(int i = start; i<=end; i++) {
+		for(int i = start; i<end; i++) // 왜 end 안넣? >> 배열에서 end는 끝나는 시간이기 때문에 넣어주지 X
 			arr[i]++;
-		}
 		
 	}
 }
