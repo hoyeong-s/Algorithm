3401196c9ce815bfbb06fea675fd4845401d9c6f
hoyeong-s <71427588+hoyeong-s@users.noreply.github.com>
2021-12-28 18:51:57 +0900

    1228

--- Prgrammers/KAKAO_BLIND_RECRUITMENT_2020/기둥과_보_설치.java
+++ Prgrammers/KAKAO_BLIND_RECRUITMENT_2020/기둥과_보_설치.java
@@ -4,58 +4,63 @@ import java.io.*;
 import java.util.*;
 
 public class 기둥과_보_설치 {
-	static int [][] map;
+	static boolean [][] g,b;
+	
 	public static void main(String[] args) {
-		int [][] build_f = {{1,0,0,1},{1,1,1,1},{2,1,0,1},{2,2,1,1},{5,0,0,1},{5,1,0,1},{4,2,1,1},{3,2,1,1}};
+		int [][] build_f = {{0,0,0,1},{2,0,0,1},{4,0,0,1},{0,1,1,1},{1,1,1,1},{2,1,1,1},{3,1,1,1},{2,0,0,0},{1,1,1,0},{2,2,0,1}};
 		solution(5,build_f);
 	}
 	public static int[][] solution(int n, int[][] build_frame) {
         int[][] answer = {};
         
-        map = new int [n+1][n+1];
-        for(int [] row : map)
-        	Arrays.fill(row, -1);
-               
+        g = new boolean [n+1][n+1];
+        b = new boolean [n+1][n+1];
+        
         for(int i=0; i<build_frame.length; i++) {
-        	if(build_frame[i][3]==1) {
-        		if(build_frame[i][2]==0) gidung(build_frame[i]);
-            	else bo(build_frame[i]);
+        	int r = build_frame[i][1];
+			int c = build_frame[i][0];
+			
+        	if(build_frame[i][2]==0) { // 기둥
+        		if(build_frame[i][3]==1) { // 설치
+        			if(checkg(r,c)) g[r][c] = true;
+        		}else { // 제거
+        			g[r][c] = false;
+        			if(!delete()) g[r][c] = true;
+        		}
+        	}else { // 보
+        		if(build_frame[i][3]==1) { // 설치
+        			if(checkb(r,c)) b[r][c] = true;
+        		}else { // 제거
+        			b[r][c] = false;
+        			if(!delete()) b[r][c] = true;
+        		}
         	}
         }
         
         return make();
     }
 	
-	private static void gidung(int [] build) {
-		int r = build[1];
-		int c = build[0];
-		
-		if(r==0 || map[r-1][c]==0 || map[r-1][c]==2) {
-			if(map[r][c]==-1) map[r][c] = 0;
-			else map[r][c] = 2;
-			return;
-		}
-		if((c>0 && map[r][c-1]==1 || map[r][c-1]==2) || (c<map.length-1 && map[r][c+1]==1 || map[r][c+1]==2)){
-			if(map[r][c]==-1) map[r][c] = 0;
-			else map[r][c] = 2;
-			return;
-		}
+	private static boolean checkg(int r, int c) {
+		if(r==0) return true;
+		if(r>0 && g[r-1][c]) return true;
+		if(c>0 && b[r][c] || b[r][c-1]) return true;
+		return false;
 	}
 	
-	private static void bo(int [] build) {
-		int r = build[1];
-		int c = build[0];
-		
-		if((map[r-1][c]==0 || map[r-1][c]==2) || (c<map.length-1 && map[r-1][c+1]==0 || map[r-1][c+1]==2)) { // 한쪽 끝이 기둥
-			if(map[r][c]==-1) map[r][c] = 1;
-			else map[r][c] = 2;
-			return;
+	private static boolean checkb(int r, int c) {
+		if(r>0 && g[r-1][c] || g[r-1][c+1]) return true;
+		if(c>0 && b[r][c-1] && b[r][c+1]) return true;
+		return false;
+	}
+	
+	private static boolean delete() {
+		for(int i=0; i<g.length; i++) {
+			for(int j=0; j<g.length; j++) {
+				if(g[i][j] && !checkg(i,j)) return false;
+				if(b[i][j] && !checkb(i,j)) return false;
+			}
 		}
-		if((c>0 && map[r][c-1]==1 || map[r][c-1]==2) && (c<map.length-1 && map[r][c+1]==1 || map[r][c+1]==2)){
-			if(map[r][c]==-1) map[r][c] = 1;
-			else map[r][c] = 2;
-			return;
-		}
+		return true;
 	}
 	
 	private static int [][] make() {
@@ -68,39 +73,39 @@ public class 기둥과_보_설치 {
 					if(o1[1]<o2[1]) return -1;
 					else if(o1[1]>o2[1]) return 1;
 					else {
-						if(o1[1]<o2[1]) return -1;
+						if(o1[2]<o2[2]) return -1;
 						else return 1;
 					}
 				}
 			}
 		});
 		
-		for(int i=0; i<map.length; i++) {
-			for(int j=0; j<map.length; j++) {
-				if(map[i][j]!=-1) {
+		for(int i=0; i<g.length; i++) {
+			for(int j=0; j<g.length; j++) {
+				if(g[i][j]) {
 					int [] arr = new int [3];
-					if(map[i][j]==2) {
-						arr[0] = j;
-						arr[1] = i;
-						arr[2] = 0;
-						pq.add(arr);
-						arr[2] = 1;
-						pq.add(arr);
-					}else {
-						arr[0] = j;
-						arr[1] = i;
-						arr[2] = map[i][j];
-						pq.add(arr);
-					}
+					arr[0] = j;
+					arr[1] = i;
+					arr[2] = 0;
+					pq.add(arr);
+				}
+				if(b[i][j]) {
+					int [] arr = new int [3];
+					arr[0] = j;
+					arr[1] = i;
+					arr[2] = 1;
+					pq.add(arr);
 				}
 			}
 		}
 		
-		int [][] result = new int [pq.size()][3];
+		int size = pq.size();
+		int [][] result = new int [size][3];
 		int cnt = 0;
 		while(!pq.isEmpty()) {
 			result[cnt++] = pq.poll();
 		}
 		return result;
 	}
+	
 }
--- Prgrammers/convert.java
+++ Prgrammers/convert.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class convert {
 
 	public static void main(String[] args) {
-		String str = "[[1,0,0,1],[1,1,1,1],[2,1,0,1],[2,2,1,1],[5,0,0,1],[5,1,0,1],[4,2,1,1],[3,2,1,1]]";
+		String str = "[[0,0,0,1],[2,0,0,1],[4,0,0,1],[0,1,1,1],[1,1,1,1],[2,1,1,1],[3,1,1,1],[2,0,0,0],[1,1,1,0],[2,2,0,1]]";
 		
 		str = str.replace("[", "{");
 		str = str.replace("]", "}");
