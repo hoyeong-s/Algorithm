9cb7ff98b391bfc90579457c09107c4fbc33a2b6
hoyeong-s <71427588+hoyeong-s@users.noreply.github.com>
2021-12-30 22:24:06 +0900

    1230

--- Prgrammers/KAKAO_INTERN_2020/수식_최대화.java
+++ Prgrammers/KAKAO_INTERN_2020/수식_최대화.java
@@ -10,7 +10,7 @@ public class 수식_최대화 {
 	static Long max = 0L;
 	
 	public static void main(String[] args) {
-		String e ="0-63";
+		String e ="100-200*300-500+20";
 		solution(e);
 	}
 	
@@ -58,36 +58,31 @@ public class 수식_최대화 {
 	}
 	
 	private static void fast(char [] save) {
-		Long [] number = new Long [list1.size()];
-		for(int i=0; i<list1.size(); i++) number[i] = list1.get(i);
+		ArrayList<Long> list = new ArrayList<>();
+		for(int i=0; i<list1.size(); i++) list.add(list1.get(i));
 		
 		Queue<Character> q = new LinkedList<Character>();
 		for(int i=0; i<list2.size(); i++) q.add(list2.get(i));
 		
 		for(int i=0; i<3; i++) {
 			char c = save[i];
-			int s = 0;
-			int e = 1;
-			while(!q.isEmpty() && e<number.length) {
-				if(number[s]==0L) while(number[s]==0L) s++;
-				if(s==e) e++;
-				if(number[e]==0L) while(e<number.length && number[e]==0L) e++;
-				
+			int idx=0;
+			int size = q.size();
+			for(int j=0; j<size; j++) {
 				char tc = q.poll();
 				if(tc==c) {
-					Long a = number[s];
-					Long b = number[e];
-					number[s] = cal(a,b,c);
-					number[e] = 0L;
+					Long a = list.get(idx);
+					Long b = list.get(idx+1);
+					list.set(idx, cal(a,b,tc));
+					list.remove(idx+1);
 				}else {
 					q.add(tc);
-					s++;
+					idx++;
 				}
-				e++;
 			}
 		}
 		
-		max = Math.max(max, Math.abs(number[0]));
+		max = Math.max(max, Math.abs(list.get(0)));
 	}
 	
 	private static long cal(Long a, Long b , char c) {
