b18ac69a189d6a20a26288f742b877975ea134b9
hoyeong-s <71427588+hoyeong-s@users.noreply.github.com>
2022-01-01 19:52:35 +0900

    0101

---
+++ Prgrammers/KAKAO_INTERN_2020/경주로_건설.java
@@ -0,0 +1,15 @@
+package KAKAO_INTERN_2020;
+
+import java.io.*;
+import java.util.*;
+
+public class 경주로_건설 {
+
+	public static void main(String[] args) {
+
+	}
+	public static int solution(int[][] board) {
+        int answer = 0;
+        return answer;
+    }
+}
