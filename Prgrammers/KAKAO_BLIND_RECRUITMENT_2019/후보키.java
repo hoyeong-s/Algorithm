7477e343bfe5735b9703fb59d876f1bae477d10b
hoyeong-s <71427588+hoyeong-s@users.noreply.github.com>
2022-01-08 22:30:23 +0900

    0108

---
+++ Prgrammers/KAKAO_BLIND_RECRUITMENT_2019/후보키.java
@@ -0,0 +1,15 @@
+package KAKAO_BLIND_RECRUITMENT_2019;
+
+import java.io.*;
+import java.util.*;
+
+public class 후보키 {
+
+	public static void main(String[] args) {
+
+	}
+	public int solution(String[][] relation) {
+        int result = 0;
+        return result;
+    }
+}
