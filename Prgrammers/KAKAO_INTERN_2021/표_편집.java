836f7f38bf6e6c7f4922770676a11ecb690ecaf5
hoyeong-s <71427588+hoyeong-s@users.noreply.github.com>
2022-01-10 21:54:29 +0900

    0110

--- Prgrammers/KAKAO_INTERN_2021/표_편집.java
+++ Prgrammers/KAKAO_INTERN_2021/표_편집.java
@@ -4,18 +4,8 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class 표_편집 {
-
-	static class Node{
-		int prev, tail;
-
-		public Node(int prev, int tail) {
-			super();
-			this.prev = prev;
-			this.tail = tail;
-		}
-	}
 	
-	static Stack<Node> stack = new Stack<>();
+	static Stack<Integer> stack = new Stack<>();
 	
 	public static void main(String[] args) {
 		String [] cmd = {"D 2","C","U 3","C","D 4","C","U 2","Z","Z"};
@@ -24,6 +14,11 @@ public class 표_편집 {
 	public static String solution(int n, int k, String[] cmd) {
 		StringTokenizer st;
         String answer = "";
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<n; i++) {
+        	sb.append(i);
+        }
         
         for(int i=0; i<cmd.length; i++) {
         	String str = cmd[i];
@@ -34,7 +29,8 @@ public class 표_편집 {
         		st = new StringTokenizer(str,"U");
         		k += Integer.parseInt(st.nextToken());
         	}else if(cmd[i].charAt(0)=='C') {
-        		
+        		stack.push(sb.charAt(k)-'0');
+        		sb.deleteCharAt(k);
         	}else {
         		
         	}
