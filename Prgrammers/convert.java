e287d77af1d5367f953ad93a504b06f395549c4d
hoyeong-s <71427588+hoyeong-s@users.noreply.github.com>
2022-01-23 15:46:26 +0900

    0123

--- Prgrammers/convert.java
+++ Prgrammers/convert.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class convert {
 
 	public static void main(String[] args) {
-		String str = "[[0,1],[0,2],[1,3],[1,4],[2,5],[2,6],[3,7],[4,8],[6,9],[9,10]]";
+		String str = "";
 		
 		str = str.replace("[", "{");
 		str = str.replace("]", "}");
