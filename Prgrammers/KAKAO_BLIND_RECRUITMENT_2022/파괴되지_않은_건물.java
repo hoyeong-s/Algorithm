dd01dbddb4d868c49194bdb67b7de0d5625db8b0
hoyeong-s <71427588+hoyeong-s@users.noreply.github.com>
2022-01-27 08:34:41 +0900

    0127

--- Prgrammers/KAKAO_BLIND_RECRUITMENT_2022/파괴되지_않은_건물.java
+++ Prgrammers/KAKAO_BLIND_RECRUITMENT_2022/파괴되지_않은_건물.java
@@ -1,59 +1,45 @@
 package KAKAO_BLIND_RECRUITMENT_2022;
 
 class Solution {
-    static int result = 0;
-    static int [][] map;
-    
     public int solution(int[][] board, int[][] skill) {
         int answer = 0;
-        result = board.length * board[0].length;
-        map = board;
+        int row = board.length;
+        int col = board[0].length;
+        int [][] pref = new int [row+1][col+1];
         
         for(int i=0; i<skill.length; i++){
-            if(skill[i][0]==1) destroy(skill[i]);
-            else build(skill[i]);
+            int degree = 0;
+            int r1 = skill[i][1];
+            int c1 = skill[i][2];
+            int r2 = skill[i][3];
+            int c2 = skill[i][4];
+            if(skill[i][0]==1) degree = -skill[i][5];
+            else degree = skill[i][5];
             
-            // for(int k=0; k<map.length; k++){
-            //     for(int j=0; j<map.length; j++){
-            //         System.out.print(map[k][j]+" ");
-            //     }
-            //     System.out.println();
-            // }
-            // System.out.println();
+            pref[r1][c1] += degree;
+            pref[r1][c2+1] -= degree;
+            pref[r2+1][c1] -= degree;
+            pref[r2+1][c2+1] += degree;
         }
         
-        for(int i=0; i<map.length; i++){
-            for(int j=0; j<map[i].length; j++)
-                if(map[i][j]>=1) answer++;
+        for(int i=0; i<pref.length; i++){
+            for(int j=0; j<pref[0].length-1; j++){
+                pref[i][j+1] += pref[i][j];
+            }
         }
-            
+        
+        for(int i=0; i<pref.length-1; i++){
+            for(int j=0; j<pref[0].length; j++){
+                pref[i+1][j] += pref[i][j];
+            }
+        }
+        
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                if(board[i][j]+pref[i][j]>=1) answer++;
+            }
+        }
+        
         return answer;
-    }
-    private static void destroy(int [] skill){
-        int sr = skill[1];
-        int sc = skill[2];
-        int er = skill[3];
-        int ec = skill[4];
-        int point = skill[5];
-        
-        for(int i=sr; i<=er; i++){
-            for(int j=sc; j<=ec; j++){
-                map[i][j] -= point;
-            }
-        }
-    }
-    
-    private static void build(int [] skill){
-        int sr = skill[1];
-        int sc = skill[2];
-        int er = skill[3];
-        int ec = skill[4];
-        int point = skill[5];
-        
-        for(int i=sr; i<=er; i++){
-            for(int j=sc; j<=ec; j++){
-                map[i][j] += point;
-            }
-        }
     }
 }
