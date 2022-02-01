f99ce3f02c501e230c363131550a0cb7c8c58256
hoyeong-s <71427588+hoyeong-s@users.noreply.github.com>
2022-02-01 19:31:34 +0900

    0201

--- Prgrammers/c++/신고결과받기.cpp
+++ Prgrammers/c++/신고결과받기.cpp
@@ -10,8 +10,10 @@ using namespace std;
 vector<int> solution(vector<string> id_list, vector<string> report, int k) {
     vector<int> answer;
     int id [id_list.size()] ;
+    int result [id_list.size()];
     unordered_set <string> hash;
     unordered_map <string,int> map;
+    unordered_map <string,vector<string>> map_list;
 
     for(int i=0; i<id_list.size(); i++){
       map[id_list[i]] = i;
@@ -28,9 +30,30 @@ vector<int> solution(vector<string> id_list, vector<string> report, int k) {
       string f,t;
       ss >> f >> t;
 
+      vector<string> v;
+      if(map_list.end() != map_list.find(f)){
+        v = map_list.find(t)->second;
+      }
+      v.push_back(f);
+      map_list[f] = v;
+
       id[map.find(t)->second]++;
+      ss.clear();
     }
-    
+
+    for(int i=0; i<id_list.size(); i++){
+      if(id[i]>=k){
+        string name = id_list[i];
+        vector<string> v;
+        v = map_list.find(name)->second;
+        for(auto str : v){
+          result[map.find(str)->second]++;
+        }
+      }
+    }
+
+    for(auto a : result)
+      answer.push_back(a);
 
     return answer;
 }
