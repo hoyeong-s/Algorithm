3fdd63ed34c1cd916a4cb378e12c045b0613146d
hoyeong-s <71427588+hoyeong-s@users.noreply.github.com>
2022-02-13 19:54:38 +0900

    0213

--- Prgrammers/괄호_회전하기.java
+++ Prgrammers/괄호_회전하기.java
@@ -1,17 +1,35 @@
 import java.util.*;
 
-class Solution {
-    public int solution(String s) {
-        int answer = -1;
+public class 괄호_회전하기 {
+	public static void main(String[] args) {
+		solution("[](){}");
+	}
+    public static int solution(String s) {
+        int answer = 0;
+        
+        StringBuilder sb = new StringBuilder();
+        
+        int len = s.length();
+        sb.append(s);
+        
+        for(int i=0; i<len; i++) {
+        	char c = sb.toString().charAt(0);
+        	sb.deleteCharAt(0);
+        	sb.append(c);
+        	if(check(sb.toString())) answer ++;
+        }
         
         return answer;
     }
     
     private static boolean check(String s){
-        Stack<Character> stack = new Stack();
+        Stack<Character> stack = new Stack<>();
         for(int i=0; i<s.length(); i++){
             if(s.charAt(i)=='{' || s.charAt(i)=='[' || s.charAt(i)=='(')
                 stack.push(s.charAt(i));
+            else if(stack.size()==0 && (s.charAt(i)=='}' || s.charAt(i)==']' || s.charAt(i)==')')) {
+            	return false;
+            }
             else if(s.charAt(i)=='}' && stack.peek()=='{'){
                 stack.pop();
             }
@@ -21,6 +39,7 @@ class Solution {
             else if(s.charAt(i)==')' && stack.peek()=='('){
                 stack.pop();
             }
+            
             else{
                 return false;
             }
